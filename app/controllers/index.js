284d490282ef36c00691dd7c3b3c3ea4a4a59f1a
beeva-fernandobordallo <fernando.bordallo@beeva.com>
2015-11-24 13:47:48 +0100

    [core] - Fixed the presence system.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,21 +8,20 @@ export default Ember.Controller.extend({
   init: function(){
     var self = this;
     this._super.apply(this, arguments);
-    console.log('hello');
     var dataUrl = new window.Firebase('https://cfmcom.firebaseio.com/presence');
     dataUrl.on('value', function(snapshot){
-      if(snapshot.val()){
-        console.log(snapshot.val());
-        console.log('Online users changed!');
         var unsorted = snapshot.val();
-        var keys = Object.keys(unsorted);
-        var items = [];
-        for (var j=0; j < keys.length; j++) {
-          items[j] = unsorted[keys[j]];
-          items[j].key = keys[j];
+        if (unsorted !== null) {
+          var keys = Object.keys(unsorted);
+          var items = [];
+          for (var j=0; j < keys.length; j++) {
+            items[j] = unsorted[keys[j]];
+            items[j].key = keys[j];
+          }
+          self.set('onlineUsers', items);
+        } else {
+          self.set('onlineUsers', '');
         }
-        self.set('onlineUsers', items);
-      }
     });
   }
 });
