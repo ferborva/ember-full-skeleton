943c7b5f5b8c3a2c7d5a13ba371ab0e119771eb7
ferborva <fbordallo2@gmail.com>
2015-12-02 23:05:52 +0100

    [login-Transitions] - First play with the animate service. Initial approach seems too tedious. Plan to improve the animation functions: see difference between page entry and exit, ...

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  hasAnimated: false,
+
+  actions:{
+
+    didTransition: function(){
+      $('#login-page').removeClass('hide');
+      Ember.run.scheduleOnce('afterRender', this, function() {
+          // If the models are already cached, the element exists.
+          this.Animate.go('.page', 'fadeInUpBig').then(function(){
+          }.bind(this));
+      });
+    },
+
+    willTransition: function(transition){
+      if(!this.get('hasAnimated')){
+        transition.abort(); 
+        this.Animate.goAndHide('#login-page', 'flipOutX').then(function(){
+          this.set('hasAnimated', true);
+          transition.retry();
+        }.bind(this));
+        return true;
+      }
+      this.set('hasAnimated', false);
+      return true;
+    }
+  }
+
 });
