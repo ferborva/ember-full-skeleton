01832b962bc300f4fc097636c5992003c9323607
beeva-fernandobordallo <fernando.bordallo@beeva.com>
2015-12-03 19:47:29 +0100

    [loading] - Added loader GIFs to the assets folder and presented a base design for a simple loading page.

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,11 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  model: function(){
+    var promise = new Promise(function(resolve){
+      setTimeout(function(){
+        resolve();
+      }, 1000);
+    });
+    return promise;
+  },
+
   actions:{
 
     didTransition: function(){
       $('#login-page').removeClass('hide');
-      this.Animate.entryPage('.page', 'fadeInDownBig', 'slow');
+      this.Animate.entryPage('.page', 'fadeInDownBig', 'normal');
     },
 
     willTransition: function(transition){
