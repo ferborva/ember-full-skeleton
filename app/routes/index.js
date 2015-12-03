de626aa8aeba0d6fdf187041f2c16964c81e0f8c
ferborva <fbordallo2@gmail.com>
2015-12-03 20:15:57 +0100

    [animation] - Extended go animation method to include speed. First test with focus animations: pulse, bounce, ... examples on index page. Actions respond to click on elements: title, paragraphs, ...

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,13 @@ import Base from './base';
 
 export default Base.extend({
 
+  actions: {
 
+    focus: function(id, animation, speed){
+      id = '#' + id;
+      this.Animate.go(id,animation,speed);
+    }
+
+  }
 
 });
