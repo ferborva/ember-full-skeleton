9ab3f5017d0b9851d44a5fc77401f147bd42728d
FBV <fbordallo2@gmail.com>
2015-12-03 14:57:44 +0100

    [animation] - Entry and Exit pages with speed first approach.

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,26 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  hasAnimated: false,
-
   actions:{
 
     didTransition: function(){
       window.$('#login-page').removeClass('hide');
-      this.Animate.entryPage('.page', 'fadeInUpBig');
+      this.Animate.entryPage('.page', 'fadeInDownBig', 'slow');
     },
 
     willTransition: function(transition){
-      if(!this.get('hasAnimated')){
-        transition.abort();
-        this.Animate.goAndHide('#login-page', 'flipOutX').then(function(){
-          this.set('hasAnimated', true);
-          transition.retry();
-        }.bind(this));
-        return true;
-      }
-      this.set('hasAnimated', false);
-      return true;
+      this.Animate.exitPage('.page','fadeOutUpBig', transition, 'slow');
     }
   }
 
