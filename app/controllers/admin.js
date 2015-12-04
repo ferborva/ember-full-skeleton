61613495fbff7d84ccc556f99a4ebd7e8d92353a
ferborva <fbordallo2@gmail.com>
2015-12-04 18:35:21 +0100

    [admin] - Created the admin controller and defined an onlineUsers property, bound to the data found on Firebase.

---
+++ app/controllers/admin.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  onlineUsers: '',
+
+  init: function(){
+    this._super.apply(this, arguments);
+
+    this.Data.get('presentUsersRef').on('value', function(snapshot){
+        if (snapshot.val() !== null) {
+          var arrData = this.Data.objectToArray(snapshot.val());
+          this.set('onlineUsers', arrData);
+        } else {
+          this.set('onlineUsers', '');
+        }
+    }.bind(this));
+
+  }
+});
