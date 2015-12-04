b8d6fc9002e6795020352d66792425674f92c825
FBV <fbordallo2@gmail.com>
2015-12-04 17:44:18 +0100

    [baseRoute] - Extended the base route properties. Now include authentication toggle and securityLevel configuration. The firebase rules and structure to the data security system will be explained in the docs.

--- app/routes/base.js
+++ app/routes/base.js
@@ -2,15 +2,40 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  // BASE ROUTE PROPERTIES: Define the public configuration variables
+
+  auth: false,
+  securityLevel: '0',
+
+
+
   beforeModel: function(transition){
-    // Check User
-    return this.Data.checkUser(transition).then(function(){
-      console.log('Move along sir ...');
-    }, function(){
-      this.Toast.addToast(this.get('i18n').t('error.notLogged'), 3000);
-      this.transitionTo('login');
-      this.send('loading');
+
+    var promise = new Promise(function (resolve, reject) {
+
+      // Check authetication toggle
+      if(this.get('auth') === false){
+        resolve('Public access');
+        return true;
+      }
+
+      this.Data.checkUser().then(function(){
+        this.Data.checkSecurityLevel(this.get('securityLevel')).then(function(){
+          console.log('SecurityLevel Cleared');
+          resolve('Security cleared');
+        }, function(){
+          console.log('User does not have clearance permission');
+          resolve('Permission denied');
+          this.transitionTo('login');
+          return true;
+        }.bind(this));
+      }.bind(this), function(){
+        this.Toast.addToast(this.get('i18n').t('error.notLogged'), 3000);
+        this.transitionTo('login');
+      }.bind(this));
     }.bind(this));
+
+    return promise;
   },
 
   actions: {
