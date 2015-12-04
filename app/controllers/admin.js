194da7b79bf9fb228e9c264b50f00c913372b829
ferborva <fbordallo2@gmail.com>
2015-12-04 20:24:58 +0100

    [dataControl] - Firsts tests on data access, security level, writting, loading and syncing.

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   onlineUsers: '',
   existingUsers: '',
+  tempUser: '',
 
   init: function(){
     this._super.apply(this, arguments);
@@ -16,15 +17,33 @@ export default Ember.Controller.extend({
         }
     }.bind(this));
 
-console.log('here');
     this.Data.get('baseRef').child('users').once('value', function(snapshot){
         if (snapshot.val() !== null) {
           var arrData = this.Data.objectToArray(snapshot.val());
+          for (var i = 0; i < arrData.length; i++) {
+            arrData[i].editting = false;
+          }
           this.set('existingUsers', arrData);
         } else {
           this.set('existingUsers', '');
         }
     }.bind(this));
 
+  },
+
+  actions: {
+    updateDisplayName: function(user){
+      this.Data.get('baseRef').child('users').child(user.key).set(user);
+    },
+
+    toggleEdit: function(user){
+      this.set('tempUser', user);
+      console.log(user);
+      if(!user.editting){
+        this.set('tempUser.editting', true);
+      }else{
+        this.set('tempUser.editting', false);
+      }
+    }
   }
 });
