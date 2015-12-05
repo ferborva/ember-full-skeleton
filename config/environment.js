d6d75f48fa1160a187e8dae1c80d7700fd0e9bc3
ferborva <fbordallo2@gmail.com>
2015-12-05 17:09:00 +0100

    [core] - Changed project name from silly misstyped fameskeleton to FlameStack.

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'fameskeleton',
+    modulePrefix: 'flamestack',
     environment: environment,
     contentSecurityPolicy: {
       'default-src': "'none'",
