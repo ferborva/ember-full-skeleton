55a87bc36e2ac062d5f9b2285f11664a10ea9174
ferborva <fbordallo2@gmail.com>
2015-12-05 16:04:41 +0100

    [admin] - Created a role level helper to present a user category for each user level found.

---
+++ tests/unit/helpers/role-level-test.js
@@ -0,0 +1,10 @@
+import { roleLevel } from '../../../helpers/role-level';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | role level');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = roleLevel(42);
+  assert.ok(result);
+});
