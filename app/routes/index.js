50dfc207ec4ab4c94a6c15644efb124a6f63ba7a
FBV <fbordallo2@gmail.com>
2015-12-06 02:29:21 +0100

    [user] - Given some time to making the user dashboard a little prettier and adding a cancel button to the new input page.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,6 @@ import Base from './base';
 
 export default Base.extend({
 
-  auth: true,
-
-  actions: {
-
-    focus: function(id, animation, speed){
-      id = '#' + id;
-      this.Animate.go(id,animation,speed);
-    }
-
-  }
+  auth: true
 
 });
