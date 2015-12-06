02a84807ad43592ddd84a2a070fe744d97642618
ferborva <fbordallo2@gmail.com>
2015-12-06 22:20:59 +0100

    [dashboar] - First attempt to bring in summary data.

--- app/routes/user/dashboard.js
+++ app/routes/user/dashboard.js
@@ -5,8 +5,44 @@ export default Base.extend({
 
   model: function(){
     var promise = new Promise(function(resolve, reject){
-      resolve();
-    });
+      this.Data.get('userRef').child('datos').once('value', function(snap){
+        var fullData = snap.val();
+        if(fullData === null){
+          resolve({data: false});
+          return;
+        }
+        if(fullData.gastos){
+          var gastos = this.Data.objectToArray(fullData.gastos);
+        }else{
+          var gastos = null;
+        }
+
+        if(fullData.ingresos){
+          var ingresos = this.Data.objectToArray(fullData.ingresos);
+        }else{
+          var ingresos = null;
+        }
+
+        var totalGastos = 0,
+            totalIngresos = 0;
+
+        if(gastos){
+          for (var i = 0; i < gastos.length; i++) {
+            totalGastos += Number(gastos[i].importe);
+          }
+        }
+
+        if(ingresos){
+          for (var i = 0; i < ingresos.length; i++) {
+            totalIngresos += ingresos[i].importe;
+          }
+        }
+        resolve({totalGastos: totalGastos, totalIngresos: totalIngresos});
+      }.bind(this), function(err){
+        console.log(err);
+        return false;
+      }.bind(this));
+    }.bind(this));
 
     return promise;
   },
