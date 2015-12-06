2a7dee5b635ccdc7faabfe3de2e57a2cf71f14df
ferborva <fbordallo2@gmail.com>
2015-12-06 11:33:34 +0100

    [minoFixes]

--- app/controllers/user/configuration.js
+++ app/controllers/user/configuration.js
@@ -5,6 +5,23 @@ export default Ember.Controller.extend({
   configValues: '',
   tempCat: '',
 
+  init: function(){
+
+    this.Data.get('userRef').child('config').on('value', function(snap){
+      if(!snap.val()){
+        this.Data.minConfigCategorias();
+      }
+      var arrData = this.Data.objectToArray(snap.val().categorias);
+      var data = snap.val();
+      data.categorias = arrData;
+      this.set('configValues', data);
+      return true;
+    }.bind(this), function(err){
+      console.log(err);
+      return false;
+    }.bind(this));
+  },
+
   actions: {
     borrarCategoria: function(categoria){
       var viejoSet = this.get('configValues.categorias');
--- app/routes/user/configuration.js
+++ app/routes/user/configuration.js
@@ -2,23 +2,6 @@ import Base from './../base';
 
 export default Base.extend({
 
-  auth: true,
-
-  model: function(){
-
-    this.Data.get('userRef').child('config').on('value', function(snap){
-      if(!snap.val()){
-        this.Data.minConfigCategorias();
-      }
-      var arrData = this.Data.objectToArray(snap.val().categorias);
-      var data = snap.val();
-      data.categorias = arrData;
-      this.controller.set('configValues', data);
-      return true;
-    }.bind(this), function(err){
-      console.log(err);
-      return false;
-    }.bind(this));
-  }
+  auth: true
 
 });
