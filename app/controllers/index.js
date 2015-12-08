508f7e2ef9311362e572676491d56b6711f64f0a
ferborva <fbordallo2@gmail.com>
2015-12-08 23:41:09 +0100

    [core] - Example use of the new datapoint method with userPresence. Found out that the aliased properties don't automatically update when the aliased object changes.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,20 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  onlineUsers: '',
 
   Data: Ember.inject.service('datapoint'),
+  onlineUsers: Ember.computed.alias('Data.onlineUsers'),
 
   init: function(){
     this._super.apply(this, arguments);
-
-    this.Data.openSocket('presentUsersRef', [], 'onlineUsers').then(
-      function (data) {
-        this.set('onlineUsers', data);
-      }.bind(this),
-      function (errorObj) {
-        console.log(errorObj);
-      }.bind(this)
-    );
   }
 });
