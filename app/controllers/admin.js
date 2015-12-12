d673b7b3725932d2a9b5e442bb95c01cbd2873a8
ferborva <fbordallo2@gmail.com>
2015-12-12 01:42:25 +0100

    [realtime] - Real time example fixed to work with on child added and removed listeners. Next add the turnFireOff usage.

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -3,9 +3,71 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   Data: Ember.inject.service('datapoint'),
-  onlineUsers: Ember.computed.alias('this.Data.onlineUsers'),
+  onlineUsers: [],
   existingUsers: Ember.computed.alias('this.Data.existingUsers'),
   tempUser: '',
+
+  init: function () {
+
+
+    this.Data.get('baseRef').child('presence').on('child_added', function(newData, previousDataId){
+      //Add/process new data
+      console.log('child added admin');
+
+      var arrData = [];
+      var values = newData.val();
+      var keys = Object.keys(values);
+      if(typeof keys[0] == 'object'){
+        arrData = this.Data.objectToArray(values);
+      }else{
+        values.key = newData.key();
+        arrData.push(values);
+      }
+      if(previousDataId === null){
+        this.set('onlineUsers', []);
+      }
+      for (var i = 0; i < arrData.length; i++) {
+        if(this.get('onlineUsers')){
+          this.get('onlineUsers').addObject(arrData[i]);
+        }else{
+          this.set('onlineUsers',arrData[i]);
+        }
+      }
+
+      this.Toast.addToast('New data recevied', 2000);
+    }.bind(this), function(error){
+      //Display error
+    }.bind(this));
+
+
+
+    this.Data.get('baseRef').child('presence').on('child_removed', function(deletedData){
+      //Remove/process old data
+      console.log('child removed admin');
+      console.log(deletedData);
+
+      var arrData = [];
+      var values = deletedData.val();
+      var keys = Object.keys(values);
+      if(typeof keys[0] == 'object'){
+        arrData = this.Data.objectToArray(values);
+      }else{
+        values.key = deletedData.key();
+        arrData.addObject(values);
+      }
+      var tempusers = this.get('onlineUsers');
+      for (var j = 0; j < arrData.length; j++) {
+        for (var i = 0; i < tempusers.length; i++) {
+          if (tempusers[i].key === arrData[j].key) {
+            tempusers.removeAt(i);
+          }
+        }
+      }
+      this.Toast.addToast('Data deleted recevied', 2000);
+    }.bind(this), function(error){
+      //Display error
+    }.bind(this));
+  },
 
   actions: {
     updateDisplayName: function(user){
